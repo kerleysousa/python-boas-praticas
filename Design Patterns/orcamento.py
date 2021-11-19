74fcbff9560f1c315b5419088a8be2e6dc88bc11
Kerley <kerley_sousa@live.com>
2021-11-18 21:38:13 -0300

    Criando estados para o orçamento.

--- Design Patterns/orcamento.py
+++ Design Patterns/orcamento.py
@@ -1,14 +1,32 @@
 class Orcamento(object):
 
+    EM_APROVACAO = 1
+    APROVADO = 2
+    REPROVADO = 3
+    FINALIZADO = 4
+
     def __init__(self):
         self.__itens = []
+        self.estado_atual = Orcamento.EM_APROVACAO
+        self.__desconto_extra = 0
+
+    def aplica_desconto_extra(self):
+        if self.estado_atual == Orcamento.EM_APROVACAO:
+            self.__desconto_extra += self.valor *0.02
+        if self.estado_atual == Orcamento.APROVADO:
+            self.__desconto_extra += self.valor *0.05
+        if self.estado_atual == Orcamento.REPROVADO:
+            raise Exception("Orçamentos reprovados não recebem desconto extra.")
+        if self.estado_atual == Orcamento.FINALIZADO:
+            raise Exception("Orçamentos reprovados não recebem desconto extra.")
+
 
     @property
     def valor(self):
         total = 0.0
         for item in self.__itens:
             total += item.valor
-        return total
+        return total - self.__desconto_extra
     
     def obter_itens(self):
         return tuple(self.__itens)
@@ -35,3 +53,17 @@ class Item(object):
         return self.__valor
     
 
+if __name__ == "__main__":
+
+    orcamento = Orcamento()
+    orcamento.adiciona_itens(Item("Item 1", 100))
+    orcamento.adiciona_itens(Item("Item 2", 200))
+    orcamento.adiciona_itens(Item("Item 3", 300))
+
+    print(orcamento.valor)
+    orcamento.aplica_desconto_extra()
+    print(orcamento.valor)
+    orcamento.estado_atual = Orcamento.APROVADO
+    orcamento.aplica_desconto_extra()
+    print(orcamento.valor)
+    print("\U0001f600")
